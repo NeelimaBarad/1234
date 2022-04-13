a11a5abe5ce500060c26b8ee97a9926af5b2f7b2
NeelimaBarad <103579107+NeelimaBarad@users.noreply.github.com>
2022-04-13 18:24:29 +0530

    Update HomePage.java

--- src/pom/HomePage.java
+++ src/pom/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-	
+	//mmmmmmmmmytydyyd
 	@FindBy(xpath="//*[@id='welcome']")
 	private WebElement welcome;
 	
